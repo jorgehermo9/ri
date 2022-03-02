4df19e784c7580ff4104617ea973a3050027628d
Jorge Hermo Gonzalez <jorge.hermo.gonzalez@udc.es>
2022-03-02 23:33:18 +0100

    fast committed

--- mri-indexer/src/main/java/es/udc/ri/IndexFiles.java
+++ mri-indexer/src/main/java/es/udc/ri/IndexFiles.java
@@ -1,11 +1,14 @@
 package es.udc.ri;
+
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.DateTools;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.LongPoint;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
+import org.apache.lucene.document.DateTools.Resolution;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
@@ -14,21 +17,30 @@ import org.apache.lucene.index.Term;
 import org.apache.lucene.index.IndexWriterConfig.OpenMode;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.store.MMapDirectory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.InetAddress;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.DirectoryStream;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
 import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class IndexFiles {
+
 
     private IndexFiles(){}
 
@@ -68,6 +80,7 @@ public class IndexFiles {
                     + "' does not exist or is not readable, please check the path");
             System.exit(1);
         }
+
         Date start = new Date();
         try {
             System.out.println("Indexing to directory '" + indexPath + "'...");
@@ -84,9 +97,43 @@ public class IndexFiles {
 
 
             try (IndexWriter writer = new IndexWriter(dir, iwc)) {
+				final int numCores = Runtime.getRuntime().availableProcessors();
+				final ExecutorService executor = Executors.newFixedThreadPool(numCores);
+				try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(docDir)) {
 
-				IndexFiles indexFiles = new IndexFiles();
-                indexFiles.indexDocs(writer, docDir);
+					/* We process each subfolder in a new thread. */
+					for (final Path path : directoryStream) {
+						if (Files.isDirectory(path)) {
+							final Runnable worker = new WorkerThread(path,writer);
+							/*
+							 * Send the thread to the ThreadPool. It will be processed eventually.
+							 */
+							executor.execute(worker);
+						}//Pensar manera de guardar los paths que no recorro y llamar a un thread que los recorra al final
+						//Por ejemplo, crear un nuevo worker sobre el docDir, con profundidad 0(o 1, la que funcione)
+					}
+		
+				} catch (final IOException e) {
+					e.printStackTrace();
+					System.exit(-1);
+				}
+		
+				/*
+				 * Close the ThreadPool; no more jobs will be accepted, but all the previously
+				 * submitted jobs will be processed.
+				 */
+				executor.shutdown();
+		
+				/* Wait up to 1 hour to finish all the previously submitted jobs */
+				try {
+					executor.awaitTermination(1, TimeUnit.HOURS);
+				} catch (final InterruptedException e) {
+					e.printStackTrace();
+					System.exit(-2);
+				}
+		
+				System.out.println("Finished all threads");
+
             }
 
             Date end = new Date();
@@ -101,13 +148,44 @@ public class IndexFiles {
 
     }
 
-	void indexDocs(final IndexWriter writer, Path path) throws IOException {
+
+}
+
+class WorkerThread implements Runnable{
+	private Path docsFolder;
+	private IndexWriter writer;
+
+	public WorkerThread(Path docsFolder,MMapDirectory indexFolder, IndexWriterConfig iwc) {
+		// El indexWriterConfig se debería poder compartir entre distintos writers
+		this.docsFolder = docsFolder;
+		try{
+			this.writer = new IndexWriter(indexFolder, iwc);
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public WorkerThread(Path docsFolder,IndexWriter writer){
+		this.docsFolder = docsFolder;
+		this.writer = writer;
+	}
+	@Override
+	public void run(){
+		try{
+			this.indexDocs();
+		}catch (IOException e){
+			e.printStackTrace();
+		}
+	}
+
+	private void indexDocs() throws IOException {
+		Path path = this.docsFolder;
 		if (Files.isDirectory(path)) {
+			// Este método tiene una  opción para el maxDepth
 			Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
 				@Override
 				public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
 					try {
-						indexDoc(writer, file, attrs.lastModifiedTime().toMillis());
+						indexDoc(file, attrs);
 					} catch (IOException ex) {
 						ex.printStackTrace(System.err);
 						// don't index files that can't be read.
@@ -115,23 +193,73 @@ public class IndexFiles {
 					return FileVisitResult.CONTINUE;
 				}
 			});
-		} else {
-			indexDoc(writer, path, Files.getLastModifiedTime(path).toMillis());
+		} else {//En teoría no se analizan los archivos normales del primer nivel. Esto es por si acaso.
+			BasicFileAttributes fileAttrs = Files.readAttributes(path,BasicFileAttributes.class);
+			indexDoc(path, fileAttrs);
 		}
 	}
 
 	/** Indexes a single document */
-	void indexDoc(IndexWriter writer, Path file, long lastModified) throws IOException {
+	private void indexDoc(Path file, BasicFileAttributes attrs) throws IOException {
+		IndexWriter writer = this.writer;
+
 		try (InputStream stream = Files.newInputStream(file)) {
 			Document doc = new Document();
 
 			Field pathField = new StringField("path", file.toString(), Field.Store.YES);
 			doc.add(pathField);
 
-			doc.add(new LongPoint("modified", lastModified));
+			doc.add(new LongPoint("modified", attrs.lastModifiedTime().toMillis()));
 
-			doc.add(new TextField("contents",
-					new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8))));
+			String content = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8)).lines().collect(Collectors.joining("\n"));
+			doc.add(new TextField("contents",content, Field.Store.NO)); //Original contents field
+
+			// Preguntar si tokenizar esto o no (En caso de que no, utilizar StringField)
+			doc.add(new TextField("contentsStored",content, Field.Store.YES)); // Contents stored
+
+			String hostname = InetAddress.getLocalHost().getHostName();
+			doc.add(new StringField("hostname",hostname, Field.Store.YES));
+
+			String thread = Thread.currentThread().getName();
+			doc.add(new StringField("thread",thread, Field.Store.YES));
+
+			String type;
+			if(attrs.isRegularFile())
+				type = "regular";
+			else if(attrs.isDirectory())
+				type = "directory";
+			else if(attrs.isSymbolicLink())
+				type = "symbolic";
+			else //if(attrs.isOther())
+				type = "other";
+
+			doc.add(new StringField("type",type, Field.Store.YES));
+
+			// El nombre debería ser sizeKB en el field...son bytes, no bits
+			// Divido entre 1000 y no entre 1024. Para que fuese entre 1024, deberían pedirse Kibibytes.
+			long sizeKb = attrs.size()/1000;
+			doc.add(new LongPoint("sizeKb", sizeKb));
+
+
+			FileTime creationTime = attrs.creationTime();
+			doc.add(new StringField("creationTime",creationTime.toString(), Field.Store.YES));
+
+			FileTime lastAccessTime = attrs.lastAccessTime();
+			doc.add(new StringField("lastAccessTime",lastAccessTime.toString(), Field.Store.YES));
+
+			FileTime lastModifiedTime = attrs.lastModifiedTime();
+			doc.add(new StringField("lastModifiedTime",lastModifiedTime.toString(), Field.Store.YES));
+
+
+			String creationTimeLucene = DateTools.dateToString(new Date(creationTime.toMillis()),Resolution.MILLISECOND);
+			doc.add(new StringField("creationTimeLucene",creationTimeLucene, Field.Store.YES));
+
+			String lastAccessTimeLucene = DateTools.dateToString(new Date(lastAccessTime.toMillis()),Resolution.MILLISECOND);
+			doc.add(new StringField("lastAccessTimeLucene",lastAccessTimeLucene, Field.Store.YES));
+
+			String lastModifiedTimeLucene = DateTools.dateToString(new Date(lastModifiedTime.toMillis()),Resolution.MILLISECOND);
+			doc.add(new StringField("lastModifiedTimeLucene",lastModifiedTimeLucene, Field.Store.YES));
+			
 
 
 			if (writer.getConfig().getOpenMode() == OpenMode.CREATE) {
