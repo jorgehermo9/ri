0ca0730a485fe5f1a481ba20fc915f664be2c869
David <david.rbacelar@udc.es>
2022-03-07 17:50:20 +0100

    Formatting

--- mri-indexer/src/main/java/es/udc/ri/IndexFiles.java
+++ mri-indexer/src/main/java/es/udc/ri/IndexFiles.java
@@ -44,15 +44,15 @@ public class IndexFiles {
 
     private IndexFiles(){}
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         String usage = "java org.apache.lucene.demo.IndexFiles"
                 + " [-index INDEX_PATH] [-docs DOCS_PATH] [-update]\n\n"
                 + "This indexes the documents in DOCS_PATH, creating a Lucene index"
                 + "in INDEX_PATH that can be searched with SearchFiles\n";
-        String indexPath ="/tmp/index";
+        String indexPath = "/tmp/index";
         String docsPath = null;
         boolean create = true;
-        for (int i = 0; i < args.length; i++) {
+        for ( int i = 0; i < args.length; i++ ) {
             switch (args[i]) {
                 case "-index":
                     indexPath = args[++i];
@@ -70,12 +70,12 @@ public class IndexFiles {
                     throw new IllegalArgumentException("unknown parameter " + args[i]);
             }
         }
-        if (docsPath == null) {
+        if ( docsPath == null ) {
             System.err.println("Usage: " + usage);
             System.exit(1);
         }
         final Path docDir = Paths.get(docsPath);
-        if (!Files.isReadable(docDir)) {
+        if ( !Files.isReadable(docDir) ) {
             System.out.println("Document directory '" + docDir.toAbsolutePath()
                     + "' does not exist or is not readable, please check the path");
             System.exit(1);
@@ -89,42 +89,31 @@ public class IndexFiles {
             Analyzer analyzer = new StandardAnalyzer();
             IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
 
-            if (create) {
+            if ( create ) {
                 iwc.setOpenMode(OpenMode.CREATE);
             } else {
                 iwc.setOpenMode(OpenMode.CREATE_OR_APPEND);
             }
 
-
             try (IndexWriter writer = new IndexWriter(dir, iwc)) {
 				final int numCores = Runtime.getRuntime().availableProcessors();
 				final ExecutorService executor = Executors.newFixedThreadPool(numCores);
 				try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(docDir)) {
-
 					/* We process each subfolder in a new thread. */
-					for (final Path path : directoryStream) {
-						if (Files.isDirectory(path)) {
+					for ( final Path path : directoryStream ) {
+						if ( Files.isDirectory(path) ) {
 							final Runnable worker = new WorkerThread(path,writer);
-							/*
-							 * Send the thread to the ThreadPool. It will be processed eventually.
-							 */
 							executor.execute(worker);
 						}//Pensar manera de guardar los paths que no recorro y llamar a un thread que los recorra al final
 						//Por ejemplo, crear un nuevo worker sobre el docDir, con profundidad 0(o 1, la que funcione)
 					}
-		
 				} catch (final IOException e) {
 					e.printStackTrace();
 					System.exit(-1);
 				}
 		
-				/*
-				 * Close the ThreadPool; no more jobs will be accepted, but all the previously
-				 * submitted jobs will be processed.
-				 */
 				executor.shutdown();
 		
-				/* Wait up to 1 hour to finish all the previously submitted jobs */
 				try {
 					executor.awaitTermination(1, TimeUnit.HOURS);
 				} catch (final InterruptedException e) {
@@ -133,37 +122,28 @@ public class IndexFiles {
 				}
 		
 				System.out.println("Finished all threads");
-
             }
-
             Date end = new Date();
 			
             try (IndexReader reader = DirectoryReader.open(dir)) {
                 System.out.println("Indexed " + reader.numDocs() + " documents in " + (end.getTime() - start.getTime())
-                        + " milliseconds");
+                    + " milliseconds");
             }
         } catch (IOException e) {
             System.out.println(" caught a " + e.getClass() + "\n with message: " + e.getMessage());
         }
-
     }
-
-
 }
 
-class WorkerThread implements Runnable{
+class WorkerThread implements Runnable {
 	private Path docsFolder;
 	private IndexWriter writer;
 
-	public WorkerThread(Path docsFolder,MMapDirectory indexFolder, IndexWriterConfig iwc) {
-		// El indexWriterConfig se debería poder compartir entre distintos writers
-		// Problema: El indexWriterConfig tiene un metodo SetWriter que hace que la config
-		// ya esté siendo usada. Crearse una config nueva, pasando como parámetro el
-		//Analyzer y el modo de apertura de la config?
+	public WorkerThread(Path docsFolder, MMapDirectory indexFolder, IndexWriterConfig iwc) {
 		this.docsFolder = docsFolder;
 		try{
 			this.writer = new IndexWriter(indexFolder, iwc);
-		}catch (IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
@@ -171,18 +151,19 @@ class WorkerThread implements Runnable{
 		this.docsFolder = docsFolder;
 		this.writer = writer;
 	}
+
 	@Override
-	public void run(){
+	public void run() {
 		try{
 			this.indexDocs();
-		}catch (IOException e){
+		} catch (IOException e){
 			e.printStackTrace();
 		}
 	}
 
 	private void indexDocs() throws IOException {
 		Path path = this.docsFolder;
-		if (Files.isDirectory(path)) {
+		if ( Files.isDirectory(path) ) {
 			// Este método tiene una  opción para el maxDepth
 			Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
 				@Override
@@ -191,7 +172,6 @@ class WorkerThread implements Runnable{
 						indexDoc(file, attrs);
 					} catch (IOException ex) {
 						ex.printStackTrace(System.err);
-						// don't index files that can't be read.
 					}
 					return FileVisitResult.CONTINUE;
 				}
@@ -202,7 +182,6 @@ class WorkerThread implements Runnable{
 		}
 	}
 
-	/** Indexes a single document */
 	private void indexDoc(Path file, BasicFileAttributes attrs) throws IOException {
 		IndexWriter writer = this.writer;
 
@@ -215,23 +194,23 @@ class WorkerThread implements Runnable{
 			doc.add(new LongPoint("modified", attrs.lastModifiedTime().toMillis()));
 
 			String content = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8)).lines().collect(Collectors.joining("\n"));
-			doc.add(new TextField("contents",content, Field.Store.NO)); //Original contents field
+			doc.add(new TextField("contents", content, Field.Store.NO)); //Original contents field
 
 			// Preguntar si tokenizar esto o no (En caso de que no, utilizar StringField)
-			doc.add(new TextField("contentsStored",content, Field.Store.YES)); // Contents stored
+			doc.add(new TextField("contentsStored", content, Field.Store.YES)); // Contents stored
 
 			String hostname = InetAddress.getLocalHost().getHostName();
-			doc.add(new StringField("hostname",hostname, Field.Store.YES));
+			doc.add(new StringField("hostname", hostname, Field.Store.YES));
 
 			String thread = Thread.currentThread().getName();
-			doc.add(new StringField("thread",thread, Field.Store.YES));
+			doc.add(new StringField("thread", thread, Field.Store.YES));
 
 			String type;
-			if(attrs.isRegularFile())
+			if ( attrs.isRegularFile() )
 				type = "regular";
-			else if(attrs.isDirectory())
+			else if ( attrs.isDirectory() )
 				type = "directory";
-			else if(attrs.isSymbolicLink())
+			else if ( attrs.isSymbolicLink() )
 				type = "symbolic";
 			else //if(attrs.isOther())
 				type = "other";
@@ -243,29 +222,26 @@ class WorkerThread implements Runnable{
 			long sizeKb = attrs.size()/1000;
 			doc.add(new LongPoint("sizeKb", sizeKb));
 
-
 			FileTime creationTime = attrs.creationTime();
-			doc.add(new StringField("creationTime",creationTime.toString(), Field.Store.YES));
+			doc.add(new StringField("creationTime", creationTime.toString(), Field.Store.YES));
 
 			FileTime lastAccessTime = attrs.lastAccessTime();
-			doc.add(new StringField("lastAccessTime",lastAccessTime.toString(), Field.Store.YES));
+			doc.add(new StringField("lastAccessTime", lastAccessTime.toString(), Field.Store.YES));
 
 			FileTime lastModifiedTime = attrs.lastModifiedTime();
-			doc.add(new StringField("lastModifiedTime",lastModifiedTime.toString(), Field.Store.YES));
+			doc.add(new StringField("lastModifiedTime", lastModifiedTime.toString(), Field.Store.YES));
 
 
-			String creationTimeLucene = DateTools.dateToString(new Date(creationTime.toMillis()),Resolution.MILLISECOND);
-			doc.add(new StringField("creationTimeLucene",creationTimeLucene, Field.Store.YES));
+			String creationTimeLucene = DateTools.dateToString(new Date(creationTime.toMillis()), Resolution.MILLISECOND);
+			doc.add(new StringField("creationTimeLucene", creationTimeLucene, Field.Store.YES));
 
-			String lastAccessTimeLucene = DateTools.dateToString(new Date(lastAccessTime.toMillis()),Resolution.MILLISECOND);
-			doc.add(new StringField("lastAccessTimeLucene",lastAccessTimeLucene, Field.Store.YES));
+			String lastAccessTimeLucene = DateTools.dateToString(new Date(lastAccessTime.toMillis()), Resolution.MILLISECOND);
+			doc.add(new StringField("lastAccessTimeLucene", lastAccessTimeLucene, Field.Store.YES));
 
-			String lastModifiedTimeLucene = DateTools.dateToString(new Date(lastModifiedTime.toMillis()),Resolution.MILLISECOND);
-			doc.add(new StringField("lastModifiedTimeLucene",lastModifiedTimeLucene, Field.Store.YES));
-			
+			String lastModifiedTimeLucene = DateTools.dateToString(new Date(lastModifiedTime.toMillis()), Resolution.MILLISECOND);
+			doc.add(new StringField("lastModifiedTimeLucene", lastModifiedTimeLucene, Field.Store.YES));
 
-
-			if (writer.getConfig().getOpenMode() == OpenMode.CREATE) {
+			if ( writer.getConfig().getOpenMode() == OpenMode.CREATE ) {
 				System.out.println("adding " + file);
 				writer.addDocument(doc);
 			} else {
