135d4d0897a1ca011c16d3409f7c6923c18c6922
Jorge Hermo Gonzalez <jorge.hermo.gonzalez@udc.es>
2022-03-16 13:11:02 +0100

    fast committed

--- README.md
+++ README.md
@@ -5,3 +5,14 @@
 ```console
 mvn exec:java -Dexec.mainClass="es.udc.ri.IndexFiles" -Dexec.args="args"
 ```
+
+```console
+mvn exec:java -Dexec.mainClass="es.udc.ri.IndexFiles" -Dexec.args="-index /tmp/index3 -docs /home/jorge/apuntes/current -openmode create -partialIndexes -numThreads 20"
+```
+```console
+mvn exec:java -Dexec.mainClass="es.udc.ri.IndexFiles" -Dexec.args="-index /tmp/index4 -docs /home/jorge/tmp/docs -openmode create -partialIndexes -numThreads 20"
+```
+
+```console
+mvn exec:java -Dexec.mainClass="es.udc.ri.StatsField" -Dexec.args="-index /tmp/index4"
+```
--- mri-indexer/src/main/java/es/udc/ri/StatsField.java
+++ mri-indexer/src/main/java/es/udc/ri/StatsField.java
@@ -2,8 +2,14 @@ package es.udc.ri;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashSet;
 
 import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.FieldInfo;
+import org.apache.lucene.index.FieldInfos;
+import org.apache.lucene.index.LeafReader;
+import org.apache.lucene.index.LeafReaderContext;
 import org.apache.lucene.search.CollectionStatistics;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.store.Directory;
@@ -34,21 +40,62 @@ public class StatsField{
         }
 
 		try{
+			//Preguntar si hace falta describir las exepciones que pueden pasar
+			//o simplemente un catch general
 			Directory dir = FSDirectory.open(Paths.get(indexPath));
 			DirectoryReader indexReader = DirectoryReader.open(dir);
 
 			IndexSearcher searcher = new IndexSearcher(indexReader);
 			
-			CollectionStatistics stats = searcher.collectionStatistics(field);
+			HashSet<String> fields = new HashSet<>();
 
-			System.out.println("Index Statistics\n");
-			System.out.println("-----------------------");
-			System.out.println("field = "+stats.field()+", docCount = "+stats.docCount()
-				+ ", maxDoc = "+stats.maxDoc()+ ", sumDocFreq = "+stats.sumDocFreq()
-				+", sumTotalFreq = "+stats.sumTotalTermFreq()
-			);
+			if (field ==null){
+				// // Preguntar si está bien recorrer todas las leaf para pillar los fields
+				// for(LeafReaderContext leaf : indexReader.leaves()){
+				// 	LeafReader leafReader = leaf.reader();
+				// 	FieldInfos fieldInfos = leafReader.getFieldInfos();
+				// 	// System.out.println("Leaf "+leaf.ord);
+				// 	// System.out.println("Numero de campos devuelto por leafReader.getFieldInfos() = " + fieldInfos.size());
+				// 	// for(FieldInfo fieldInfo: fieldInfos){
+				// 	// 	System.out.print(fieldInfo.name+",");
+				// 	// }
+				// 	for(FieldInfo fieldInfo: fieldInfos){
+				// 		fields.add(fieldInfo.name);
+				// 	}
+				// }
 
+				FieldInfos fieldInfos = FieldInfos.getMergedFieldInfos(indexReader);
+				for(FieldInfo fieldInfo: fieldInfos){
+					fields.add(fieldInfo.name);
+				}
+			}else{
+				fields.add(field);
+			}
+			System.out.println("\nIndex Statistics");
 
+			System.out.printf("%-80s\n","-".repeat(80));
+
+			System.out.printf("|%-23s | ","field");
+			System.out.printf("%-8s | ","docCount");
+			System.out.printf("%-8s | ","maxDoc");
+			System.out.printf("%-10s | ","sumDocFreq");
+			System.out.printf("%-17s|\n","sumCollectionFreq");
+
+			System.out.printf("%-80s\n","-".repeat(80));
+			for(String f: fields){
+				CollectionStatistics stats = searcher.collectionStatistics(f);
+				// Si no hay estadísticas para ese campo, por ejemplo, los LongPoint no indexan términos
+				if (stats == null){continue;}
+				System.out.printf("|%-23s | ",stats.field());
+				System.out.printf("%-8d | ",stats.docCount());
+				System.out.printf("%-8d | ",stats.maxDoc());
+				System.out.printf("%-10d | ",stats.sumDocFreq());
+				System.out.printf("%-17d|\n",stats.sumTotalTermFreq());
+			}
+			System.out.printf("%-80s\n","-".repeat(80));
+
+			dir.close();
+			indexReader.close();
 		}catch (IOException e) {
             System.err.println(" caught a " + e.getClass() + "\n with message: " + e.getMessage());
             System.exit(1);
