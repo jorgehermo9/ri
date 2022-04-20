7f931652ccedcc22b5b92716049454905122f990
Jorge Hermo Gonzalez <jorge.hermo.gonzalez@udc.es>
2022-04-20 14:21:42 +0200

    fast committed

--- mri-searcher/src/main/java/es/udc/ri/SearchEvalMedline.java
+++ mri-searcher/src/main/java/es/udc/ri/SearchEvalMedline.java
@@ -2,15 +2,42 @@ package es.udc.ri;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.search.similarities.ClassicSimilarity;
 import org.apache.lucene.search.similarities.LMJelinekMercerSimilarity;
 import org.apache.lucene.search.similarities.Similarity;
+import org.apache.lucene.store.FSDirectory;
 
+
+class QueryMetrics{
+	MedlineInfo query;
+	double precision;
+	double recall;
+	double ap;
+
+	QueryMetrics(MedlineInfo query, double precision, double recall, double ap){
+		this.query = query;
+		this.precision = precision;
+		this.recall = recall;
+		this.ap = ap;
+	}
+	public MedlineInfo getQuery() { return this.query; }
+	public double getPrecision() { return this.precision; }
+	public double getRecall() { return this.recall; }
+	public double getAp() { return this.ap; }
+}
 public class SearchEvalMedline{
 	public static void main(String[] args) {
 		String usage = "java es.udc.ri.SearchEvalMedline"
@@ -125,19 +152,160 @@ public class SearchEvalMedline{
 		HashMap<Long,List<Long>> relevances = MedlineInfo.getRelevance(relevanceReader);
 
 		if( lowerBound !=null && upperBound !=null){
-			//No hacer un sublist, porque no se asegura que la lista tenga las queries ordenadas
-			//por ID, se podría ordenar antes y hacer un sublist, pero no es tan eficiente.
-			List<MedlineInfo> auxList = new ArrayList<MedlineInfo>();
-			for (MedlineInfo query : queriesInfo){
-				if (query.getId() >= lowerBound && query.getId() <= upperBound){
-					auxList.add(query);
-				}
-			}
-			queriesInfo = auxList;
+			//Hay que declararlas finale sino da error con el removeIf
+			final int lower = lowerBound;
+			final int upper = upperBound;
+			queriesInfo.removeIf(query -> query.getId() < lower || query.getId() > upper);
 		}
 
-		System.out.println(queriesInfo);
-		System.out.println(relevances.get((long) 1));
+		//Empezamos a leer el índice
 
+		IndexReader reader = null;
+		IndexSearcher searcher = null;
+		QueryParser parser;
+		
+		try {
+			reader = DirectoryReader.open(FSDirectory.open(Paths.get(indexPath)));
+
+		} catch (Exception e){
+			System.err.println("Could not read index in " + indexPath);
+			System.exit(1);
+		}
+		
+		searcher = new IndexSearcher(reader);
+		searcher.setSimilarity(similarity);
+		parser = new QueryParser("Contents", new StandardAnalyzer());
+
+		List<QueryMetrics> metrics = new ArrayList<>();
+
+		for(MedlineInfo query : queriesInfo){
+			List<Long> relevantDocs = relevances.get(query.getId());
+			QueryMetrics queryMetrics = processQuery(reader,searcher,parser,query,relevantDocs,top,cut);
+			//Si se ha podido computar los query metrics
+			if (queryMetrics != null){
+				metrics.add(queryMetrics);
+			}
+		}
+
+
+		double meanPrecision = metrics.stream().mapToDouble(metric -> metric.getPrecision()).summaryStatistics().getAverage();
+		double meanRecall = metrics.stream().mapToDouble(metric -> metric.getRecall()).summaryStatistics().getAverage();
+		double meanAp = metrics.stream().mapToDouble(metric -> metric.getAp()).summaryStatistics().getAverage();
+		System.out.println("----------------------------------------\n");
+		String message;
+		if (queries.equals("all")){
+			message = "all queries";
+		}else if(queries.contains("-")){
+			message = "queries " + queries;
+		}else{
+			message = "query " + queries;
+		}
+		System.out.println("Summary for "+message+":");
+		System.out.println("Mean P@"+cut+": "+meanPrecision);
+		System.out.println("Mean Recall@"+cut+": "+meanRecall);
+		System.out.println("MAP@"+cut+": "+meanAp);
+
+
+	}
+
+	public static QueryMetrics processQuery(IndexReader reader, IndexSearcher searcher, QueryParser parser, MedlineInfo query,List<Long> relevantDocs,int top,int cut){
+
+		//Pasar a minúsculas y eliminar paréntesis
+		String queryContents = query.getContents().toLowerCase().replace("(", "").replace(")","");
+		int numDocs = Math.max(top,cut);
+		Query parsedQuery = null;
+		try{
+			//Preguntar si quitar paréntesis. Da problemas con paréntesis no cerrados
+			parsedQuery = parser.parse(queryContents);
+		}catch (Exception e){
+			System.err.println("Could not parse query with id " + query.getId()+": "+e.getMessage());
+			return null;
+		}
+		TopDocs topdocs = null;
+		try{
+			topdocs = searcher.search(parsedQuery,numDocs);
+		}catch (Exception e){
+			System.err.println("Could not get top docs from query with id "+ query.getId()+": "+e.getMessage());
+			return null;
+		}
+
+		long totalHits = topdocs.totalHits.value;
+		ScoreDoc[] scoreDocs= topdocs.scoreDocs;
+		int VP = 0;
+		double apAux=0;
+
+		System.out.println("-------------------------");
+		System.out.println("Ranking for query "+query.getId()+": "+queryContents);
+
+		System.out.printf("%-37s\n", "-".repeat(37));
+
+		System.out.printf("|%-4s | ", "Rank");
+		System.out.printf("%-9s | ", "MedlineID");
+		System.out.printf("%-9s | ", "Score");
+		System.out.printf("%-4s|\n", "Rel");
+		System.out.printf("%-37s\n", "-".repeat(37));
+		
+		for (int i = 0; i < Math.min(totalHits,numDocs); i++) {
+			int docId = scoreDocs[i].doc;
+			try{
+				long medlineId = Long.parseLong(reader.document(docId).get("DocIDMedline"));
+				boolean relevant = relevantDocs.contains(medlineId);
+				
+				if (i<top){
+					System.out.printf("|%-4d | ", i+1);
+					System.out.printf("%-9d | ", medlineId);
+					System.out.printf("%-9.6f | ", scoreDocs[i].score);
+					System.out.printf("%-4s|\n", relevant?" R":"");
+				}
+
+				
+				if (relevant && i<cut){
+					VP++;
+					apAux += (double) VP/ (double) (i+1);
+				}
+			}catch (Exception e){
+				System.err.println("Error while reading index for document "+ docId+" in query with id "+query.getId()+": "+e.getMessage());
+				continue;
+			}
+		}
+		System.out.printf("%-37s\n", "-".repeat(37));
+
+		for(int i =0;i<Math.min(top,totalHits);i++){
+			ScoreDoc doc = scoreDocs[i];
+			int docId = doc.doc;
+			try{
+				long medlineId = Long.parseLong(reader.document(docId).get("DocIDMedline"));
+				String medlineContents = reader.document(docId).get("Contents");
+				System.out.println("Contents for document "+ medlineId+":");
+				System.out.println("\t"+medlineContents.replace("\n","\n\t"));
+
+			}catch(Exception e){
+				System.err.println("Error while reading index for document "+ docId+" in query with id "+query.getId()+": "+e.getMessage());
+				continue;
+			}
+		}
+
+
+		System.out.println("-------------------------");
+		System.out.println("Metrics for query "+query.getId()+"\n");
+
+		//Si la query no tiene relevantes, no evaluar métricas
+		if (relevantDocs.size() == 0){
+			System.err.println("Query " + query.getId()+" does not have any relevant document. Can't compute metrics.");
+			return null;
+		}
+		double precision = (double) VP / (double) Math.min(cut,totalHits);
+		double recall = (double) VP / (double) relevantDocs.size();
+		// Esta métrica así sería parecida al precision, pero penalizando más a los documentos
+		// que tienen otros no relevantes encima. No sé si estaría bien usarla tambien
+		// double ap = apAux/Math.min(cut,totalHits);
+		double ap = apAux/ (double)relevantDocs.size();
+
+		System.out.println("P@"+cut+": "+precision);
+		System.out.println("Recall@"+cut+": "+recall);
+		System.out.println("AP@"+cut+": "+ap);
+		System.out.println("----------------------------------------\n");
+
+		return new QueryMetrics(query,precision, recall, ap);
 	}
 }
